9e510085b709e821817a88f726ea0cd724383146
Victor Rodriguez <awtgerry@gmail.com>
2023-08-09 21:19:33 -0600

    Working arround this
    Find out how to do forms.

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,26 @@ fn sum(a: i32, b: i32) -> i32 {
     a + b
 }
 
-fn say_hi() {
-    let a = 1;
-    let b = 2;
+fn do_sum() {
+    let a: i32 = 0;
+    let b: i32 = 0;
     sycamore::render(|cx| view! { cx,
+        input(placeholder="Value of a") {
+            "a = " (a)
+        }
+        input(placeholder="Value of b") {
+            "b = " (b)
+        }
         p { "The sum of " (a) " and " (b) " is " (sum(a, b)) "." }
     });
 }
 
+fn say_hi() {
+    sycamore::render(|cx| view! { cx,
+        p { "hello, world!" }
+    });
+}
+
 fn main() {
-    say_hi();
+    do_sum();
 }
